b9342298c48a86b3abec2f492d7ca9d08d3c1247
ayushratan082 <135870585+AyushRatan1@users.noreply.github.com>
2024-01-31 20:10:02 +0530

    Navbar

--- assets/js/foodhub.js
+++ assets/js/foodhub.js
@@ -114,3 +114,13 @@ window.addEventListener('scroll', function () {
     }
 }
 
+
+function openPopup() {
+  // Show the popup
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  // Hide the popup
+  document.getElementById("popup").style.display = "none";
+}
